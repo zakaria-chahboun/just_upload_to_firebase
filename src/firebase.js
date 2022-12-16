69014be5bf94b75e1f5383487cb5775edb9615ff
zakaria chahboun <zakaria.chahboun.1996@gmail.com>
2022-12-16 22:06:39 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,11 +4,11 @@ import "firebase/storage";
 
 // Firebase config
 const firebaseConfig = {
-    apiKey: "AIzaSyC5F2t4D9EL8Tg4WVN2-T3PSriCL5YJfzo",
-    authDomain: "supercode-40a70.firebaseapp.com",
-    databaseURL: "https://supercode-40a70.firebaseio.com",
-    projectId: "supercode-40a70",
-    storageBucket: "supercode-40a70.appspot.com"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: ""
 };
 
 firebase.initializeApp(firebaseConfig);
